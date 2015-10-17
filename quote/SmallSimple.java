657c72e43c14dcb066c802352e3f84e104d63a0b
mehmed fatih tanış <mehmedfatih29@hotmail.com>
2015-10-17 17:48:41 +0300

    Changed the year, author and text

--- quote/SmallSimple.java
+++ quote/SmallSimple.java
@@ -2,17 +2,17 @@ package quote;
 
 public class SmallSimple implements Quotation {
     public int year() {
-        return 1973;
+        return 2005;
     }
     public String text() {
-        String p = "Small Is Beautiful -- the aim ought to be to obtain the maximum ";
-        String s = "amount of well being with the minimum amount of consumption.";
+        String p = "GIT; yazýlým geliþtirme süreçlerinde kullanýlan, hýz odaklý, daðýtýk çalýþan bir sürüm kontrol ve kaynak kod yönetim sistemidir";
+        String s = " Ýlk sürümü Linux çekirdeði'nin geliþtirilmesinde kullanýlmak üzere 2005 yýlýnda bizzat Linus Torvalds tarafýndan tasarlanýp geliþtirilmiþ";
         return p+s;
     }
     public String author() {
-        return "Schumacher";
+        return "Linus Torvalds";
     }
     public String reference() {
-        return "https://en.wikipedia.org/wiki/Small_Is_Beautiful";
+        return "https://tr.wikipedia.org/wiki/Git_(yaz%C4%B1l%C4%B1m)";
     }
 }
