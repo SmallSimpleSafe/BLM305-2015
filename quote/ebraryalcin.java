3ed4b1fbb797a25d66342a5470d625d4b57c4e4a
ebrrylcn <ebraryalcin@gmail.com>
2015-10-17 19:37:40 +0300

    ebrrylcn
    some changes

---
+++ quote/ebraryalcin.java
@@ -0,0 +1,18 @@
+package quote;
+
+public class SmallSimple implements Quotation {
+    public int year() {
+          return 1945;
+    }
+    public String text() {
+        String p = "And I can fight only for something that I love, love only what I respect, ";
+        String s = " and respect only what I at least know.” ";
+        return p+s;
+    }
+    public String author() {
+        return "Adolf Hitler";
+    }
+    public String reference() {
+        return "http://www.goodreads.com/author/quotes/30691.Adolf_Hitler";
+    }
+}
