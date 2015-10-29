304adb093d0e62d6331287536011a592c3a0f636
Akif Eyler <ma.eyler@gmail.com>
2015-10-29 08:24:06 +0300

    end of text project

---
+++ text/EdanurGur.java
@@ -0,0 +1,63 @@
+package text;
+
+import java.util.*;
+
+class EdanurGur implements Processor {
+
+    public String process(String input) {
+        char[] harf = input.toLowerCase().toCharArray();
+        List<Character> harfler = new ArrayList<>();
+        
+        for(Character t:harf){
+            harfler.add(t);
+        }
+        
+        for(int i=0; i<harfler.size(); i++){
+            if(harfler.get(i).equals('ý')){
+                harfler.set(i, 'i');
+            }
+            
+            else if(harfler.get(i).equals('ö')){
+                harfler.set(i,'o');
+            }
+            
+            else if(harfler.get(i).equals('ü')){
+                harfler.set(i,'u');
+            }
+            
+            else if(harfler.get(i).equals('ç')){
+                harfler.set(i,'c');
+            }
+            
+            else if(harfler.get(i).equals('ð')){
+                harfler.set(i,'g');
+            }
+            
+            else if(harfler.get(i).equals('þ')){
+                harfler.set(i,'s');
+            }
+            
+            else{}
+        }
+        
+        char[] again = new char[harfler.size()];
+        
+        for (int i = 0; i < again.length; i++) {
+            again[i] = harfler.get(i);
+            System.out.println(again[i]);
+        }
+        String engString = new String(again);
+        System.out.println(engString);
+        return engString;
+    }
+    
+    public String description(String source) {
+        return "Converted Turkish letters to English";
+    }
+
+    public String author() {
+        return "Edanur Gür";
+    
+    }
+}
+
